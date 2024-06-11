0eeef7c70116d29ffa3b0d80fee6d7e5c0ee5cae
Zoedube <rofhiwazoe11@gmail.com>
2024-06-11 21:38:07 +0200

    updated changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] =  React.useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.eclair} alt="app logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#About">About</a></li>
+        <li className="p__opensans"><a href="#Menu"></a>Menu</li>
+        <li className="p__opensans"><a href="#Contact"></a>Contact</li>
+      </ul>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
+         
+      {toggleMenu && (
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+          <ul className="app__navbar-smallscreen_links">
+            <li className="p__opensans"><a href="#home">Home</a></li>
+            <li className="p__opensans"><a href="#About">About</a></li>
+            <li className="p__opensans"><a href="#Menu"></a>Menu</li>
+            <li className="p__opensans"><a href="#Contact"></a>Contact</li>
+          </ul>
+        </div>
+      )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,7 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
 import G from '../assets/G.png';
+import eclair from '../assets/eclair.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
@@ -19,7 +20,6 @@ import award03 from '../assets/award03.png';
 import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
 
 export default {
   bg,
@@ -43,5 +43,5 @@ export default {
   award05,
   sign,
   quote,
-  gericht,
+  eclair,
 };
