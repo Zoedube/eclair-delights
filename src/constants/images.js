51419948ee59e1b89561638ed6c8049bcba427f4
Zoedube <rofhiwazoe11@gmail.com>
2024-06-12 22:21:57 +0200

    added pictures

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,17 +1,16 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
-import G from '../assets/G.png';
+import smoke from '../assets/smoke.png';
 import eclair from '../assets/eclair.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
 import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
+import whisk from '../assets/whisk.png';
 import logo from '../assets/logo.png';
 import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
 import findus from '../assets/findus.png';
 import laurels from '../assets/laurels.png';
 import award01 from '../assets/award01.png';
@@ -22,21 +21,27 @@ import sign from '../assets/sign.png';
 import background from '../assets/background.png';
 import header from '../assets/header.png';
 import hero from '../assets/hero.png';
+import bgimg from '../assets/bgimg.png';
+import finaloverlay from '../assets/finaloverlay.png';
+import marble from '../assets/marble.png';
+import gold from '../assets/gold.png';
+import vertical from '../assets/vertical.png';
+import choc from '../assets/choc.png';
+import flour from '../assets/flour.png';
+import strawberry from '../assets/strawberry.png';
 
 export default {
   bg,
   chef,
-  G,
+  smoke,
   gallery01,
   gallery02,
   gallery03,
   gallery04,
-  knife,
   logo,
   menu,
   overlaybg,
   spoon,
-  welcome,
   findus,
   laurels,
   award01,
@@ -48,4 +53,14 @@ export default {
   hero,
   header,
   background,
+  whisk,
+  bgimg,
+  marble,
+  finaloverlay,
+  gold,
+  vertical,
+  flour,
+  strawberry,
+  choc,
+
 };
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -4,30 +4,43 @@ import './AboutUs.css';
 
 const AboutUs = () => (
   <div className="app__aboutus app__bg flex__center section__padding" id="about">
-    <div className="app__aboutus-overlay flex__center">
-      <img src={images.G} alt="g letter" />
-      </div> 
 
-      <div className="app__aboutus-content flex__center">
-        <div className="app__aboutus-content_about">
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__image"/>
-        <p className="p__opensans">hshdgdhvjdghv</p>
+        <img src={images.spoon} alt="about_spoon" className="spoon__image" />
+        <p className="p__opensans">
+            Welcome to Eclair Delights! We are a bakery committed to offering a wide variety of delicious treats, not just eclairs. Our mission is to satisfy all your sweet cravings with our high-quality baked goods.
+            <br /><br />
+            From bread and pastries to cakes and cookies, our selection is crafted to delight your taste buds. We use only premium ingredients to ensure every bite is a moment of pure bliss.
+            <br /><br />
+            Whether you’re here for your morning coffee or a custom cake, we strive to make every visit memorable. Thank you for choosing Eclair Delights. We can't wait to share our passion for baking with you!
+        </p>
+
         <button type="button" className="custom__button">Know More</button>
-        </div>
 
-        <div className="app__aboutus-content_knife flex__center">
-        <img src={images.knife} alt="about_knife" />
-        </div>
+      </div>
 
-        <div className="app__aboutus-content_history">
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.choc} alt="about_knife" />
+      </div>
+      <div className="app__aboutus-content_history ">
         <h1 className="headtext__cormorant">Our History</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__image"/>
-        <p className="p__opensans">hshdgdhvjdghv</p>
+        <img src={images.spoon} alt="about_spoon" className="spoon__image" />
+        <p className="p__opensans">
+            Eclair Delights started with a dream by our founder, Zoe Dube, who loved baking since childhood. Inspired by family traditions, Zoe wanted to bring the joy of homemade treats to everyone.
+            <br /><br />
+            In 2010, Zoe opened Eclair Delights in a cozy neighborhood spot. With a few classic recipes, the bakery quickly became popular for its quality and welcoming vibe.
+            <br /><br />
+            Today, Eclair Delights is a beloved local bakery known for its diverse range of pastries, breads, and desserts. Despite our growth, we remain true to our roots, baking with the same love and care. We look forward to many more years of success with your support.
+        </p>
+
         <button type="button" className="custom__button">Know More</button>
-        </div>
+
       </div>
     </div>
+  </div>
 );
 
 export default AboutUs;
